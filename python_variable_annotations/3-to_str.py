2b606ef77b93b40bd841dc89fa3b0213c64e5921
Murad-Cavadov <13860@holbertonstudents.com>
2026-05-26 11:26:31 +0000

    Fix: Add task 3-to_str.py with float-to-string annotations

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module provides a type-annotated function 'to_str' that
+takes a float argument and returns its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a given float number.
+    """
+    return str(n)
