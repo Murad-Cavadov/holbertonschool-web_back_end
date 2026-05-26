ed2b1e667097e0cb5599127e328f0145224699ad
Murad-Cavadov <13860@holbertonstudents.com>
2026-05-26 11:27:14 +0000

    Fix: Add task 4-define_variables.py with annotated variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module defines and annotates several variables with specific
+types and values, including int, float, boolean, and string.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
