102e9eadfd013a78289b0eb75582ac1a875e7e98
Murad-Cavadov <13860@holbertonstudents.com>
2026-05-26 11:24:47 +0000

    Fix: Add task 1-concat.py with string type annotations

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module provides a type-annotated function 'concat' that
+takes two string arguments and returns their concatenated result.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the combined string.
+    """
+    return str1 + str2
