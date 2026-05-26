ccf671d74373ca48402748b85802e3e9f98cba44
Murad-Cavadov <13860@holbertonstudents.com>
2026-05-26 11:46:16 +0000

    Fix: Add task 4-tasks.py utilizing task_wait_random

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+This module provides an asynchronous routine 'task_wait_n' that spawns
+multiple asyncio.Tasks concurrently using 'task_wait_random'.
+"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns task_wait_random n times with the specified max_delay.
+    Returns the list of all the delays in ascending order.
+    """
+    delays = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
