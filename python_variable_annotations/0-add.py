16f283b3b76dcc639bf2da1659dfd17815f707f9
Murad-Cavadov <13860@holbertonstudents.com>
2026-05-26 11:23:32 +0000

    Fix: Add task 0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module provides a type-annotated function 'add' that
+takes two float arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Takes two float numbers and returns their sum.
+    """
+    return a + b
