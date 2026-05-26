116a27bf178251e9603072de658ba9c890151cce
Murad-Cavadov <13860@holbertonstudents.com>
2026-05-26 11:32:29 +0000

    Fix: Add task 7-to_kv.py with Tuple and Union annotations

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module provides a type-annotated function 'to_kv' that takes a
+string and an int or float, and returns a tuple.
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns a tuple where the first element is the string k, and the
+    second element is the square of the number v as a float.
+    """
+    return (k, float(v ** 2))
